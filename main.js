43b8f15bc6378b10482e5cd9a786f2b9f48d64b5
fateh-ark <akbarfatih92@gmail.com>
2022-09-06 14:47:27 +0700

    added some more vars in main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+function main(){
+    var canvas = document.getElementById("myCanvas");
+    var gl = canvas.getContext("webgl");
+
+    //vertex shader
+    var vertexShaderCode = "void main () {" + "}";
+
+    //fragment shader
+    var fragmentShaderCode = `
+        void main(){
+
+        }
+    `;
+
+}
