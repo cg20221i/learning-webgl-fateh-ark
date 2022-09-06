21f89322ee23dda09b4a96c80f5bbdbed1f69faf
fateh-ark <akbarfatih92@gmail.com>
2022-09-06 15:13:25 +0700

    Added some more stuff in main.js

--- main.js
+++ main.js
@@ -5,11 +5,27 @@ function main(){
     //vertex shader
     var vertexShaderCode = "void main () {" + "}";
 
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader,vertexShaderCode);
+    gl.compileShader(vertexShader);
+
     //fragment shader
     var fragmentShaderCode = `
         void main(){
 
         }
     `;
+    var fragmentShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(fragmentShader,fragmentShaderCode);
+    gl.compileShader(fragmentShader);
+
+    var shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram,vertexShader);
+    gl.attachShader(shaderProgram,fragmentShader);
+    gl.linkProgram(shaderProgram);
+    gl.useProgram(shaderProgram);
+
+    gl.clearColor(0.0,0.0,0.0,1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
 
 }
