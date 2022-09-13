d1caf15e881c2625bfb1a07edda9f1a3408ece3a
fateh-ark <akbarfatih92@gmail.com>
2022-09-13 13:55:23 +0700

    Add one blue point data

--- main.js
+++ main.js
@@ -1,31 +1,41 @@
-function main(){
+function main() {
     var canvas = document.getElementById("myCanvas");
     var gl = canvas.getContext("webgl");
 
-    //vertex shader
-    var vertexShaderCode = "void main () {" + "}";
-
-    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertexShader,vertexShaderCode);
-    gl.compileShader(vertexShader);
-
-    //fragment shader
-    var fragmentShaderCode = `
-        void main(){
-
+    // VERTEX SHADER
+    var vertexShaderCode =`
+        void main (){
+            gl_PoinySize = 5.0
+            gl_Position = vec4(-1.0, 1.0, 0.0, 1.0);
         }
     `;
-    var fragmentShader = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(fragmentShader,fragmentShaderCode);
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, vertexShaderCode);
+    gl.compileShader(vertexShader);
+
+    // FRAGMENT SHADER
+    var fragmentShaderCode = `
+        void main() {
+            gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+        }
+    `;
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, fragmentShaderCode);
     gl.compileShader(fragmentShader);
 
+    // Comparing to C-Programming, we may imagine
+    //  that up to this step we have created two
+    //  object files (.o), for the vertex and fragment shaders
+
     var shaderProgram = gl.createProgram();
-    gl.attachShader(shaderProgram,vertexShader);
-    gl.attachShader(shaderProgram,fragmentShader);
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
 
-    gl.clearColor(0.0,0.0,0.0,1.0);
+    gl.clearColor(1.0, 0.75,   0.79,  1.0);
+                //Red, Green, Blue, Alpha
     gl.clear(gl.COLOR_BUFFER_BIT);
 
+    gl.drawArrays(gl.POINTS, 0, 1); 
 }
