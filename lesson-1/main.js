1d6d0cb8d8f5a14c93b59ab308b2ec4942f61a07
fateh-ark <akbarfatih92@gmail.com>
2022-09-20 13:10:35 +0700

    Moved previous files to "lesson-1"

--- lesson-1/main.js
+++ lesson-1/main.js
@@ -2,26 +2,19 @@ function main() {
     var canvas = document.getElementById("myCanvas");
     var gl = canvas.getContext("webgl");
 
-    /**
-     * A (  0.5,  0.5)
-     * B (  0.0,  0.0)
-     * C ( -0.5,  0.5)
-     */
-
     var vertices = [
         -0.1,0.1,
-        -0.3,0.0,
-        -0.5,0.2,
-        -0.7,0.0,
-        -0.9,0.3,
-        0.1,0.0,
-        0.3,-0.1,
-        0.5,0.0,
-        0.7,-0.2,
-        0.9,0.0
+        -0.3,0.3,
+        -0.5,0.5,
+        -0.7,0.7,
+        -0.9,0.9,
+        0.1, 0.1,
+        0.3,-0.3,
+        0.5,-0.5,
+        0.7,-0.7,
+        0.9,0.9
     ];
 
-    // Create a linked-list for storing the vertices data in the GPU realm
     var buffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
@@ -32,8 +25,6 @@ function main() {
         void main () {
             gl_PointSize = 5.0;
             gl_Position = vec4(aPosition, 0.0, 1.0);
-            // gl_Position is the final destination for storing
-            //  positional data for the rendered vertex
         }
     `;
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
@@ -44,18 +35,11 @@ function main() {
     var fragmentShaderCode = `
         void main() {
             gl_FragColor = vec4(1.0, 0.8, 0.8, 1.0);
-            // Blue = R:0, G:0, B:1, A:1
-            // gl_FragColor is the final destination for storing
-            //  color data for the rendered fragment
         }
     `;
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShader, fragmentShaderCode);
     gl.compileShader(fragmentShader);
-
-    // Comparing to C-Programming, we may imagine
-    //  that up to this step we have created two
-    //  object files (.o), for the vertex and fragment shaders
 
     var shaderProgram = gl.createProgram();
     gl.attachShader(shaderProgram, vertexShader);
@@ -63,16 +47,12 @@ function main() {
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
 
-    // Teach the GPU how to collect
-    //  the positional values from ARRAY_BUFFER
-    //  for each vertex being processed
+    gl.clearColor(0.2, 0.1,   0.0,  1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
     var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
     gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(aPosition);
 
-    gl.clearColor(0.2, 0.1,   0.0,  1.0);
-                //Red, Green, Blue, Alpha
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
-    gl.drawArrays(gl.POINT, 0, 10);
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, 10);
 }
